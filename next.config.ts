8e18f19a09b072016f9003abbe77efa2579ddbfb
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2026-05-10 20:07:55 +0700

    delete reverse proxy url

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.restoandcafepempek19.my.id/:path*",
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
+
 };
 
 export default nextConfig;
